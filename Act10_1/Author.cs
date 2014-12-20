4be9800797a9725cf85c7a45832c561c23fa333d
johnmetzbower <johnmetzbower@nokware.net>
2014-12-20 15:57:53 -0600

    Act10_1

---
+++ Act10_1/Author.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace Act10_1
+{
+    public class Author
+    {
+
+        SqlConnection _pubConnection;
+        string _connString;
+        public Author()
+        {
+        }
+        public List<String> GetAuthorList()
+        {
+            SqlCommand authorsCommand = new SqlCommand();
+            SqlDataReader authorDataReader;
+            List<string> nameList = new List<string>();
+            try
+            {
+                authorsCommand.Connection = _pubConnection;
+                authorsCommand.CommandText = "Select au_lnamae from authors";
+                _pubConnection.Open();
+                authorDataReader = authorsCommand.ExecuteReader();
+                while (authorDataReader.Read() == true)
+                {
+                    nameList.Add(authorDataReader.GetString(0));
+                }
+                return nameList;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (_pubConnection != null)
+                {
+                    _pubConnection.Close();
+                }
+            }
+            _connString =
+                    "Data source=localhost;Initial Catalog=pubs;Integrated Security=True";
+            _pubConnection = new SqlConnection();
+            _pubConnection.ConnectionString = _connString;
+        }
+        public int CountAuthors()
+        {
+            try
+            {
+                SqlCommand pubCommand = new SqlCommand();
+                pubCommand.Connection = _pubConnection;
+                pubCommand.CommandText = "Select Count(au_id) from authors";
+                _pubConnection.Open();
+                return (int)pubCommand.ExecuteScalar();
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (_pubConnection != null)
+                {
+                    _pubConnection.Close();
+                }
+            }
+        }
+    }
+}
+    
---
+++ Act10_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Act10_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Author author = new Author();
+                foreach (String name in author.GetAuthorList())
+                {
+                    Console.WriteLine(name);
+                }
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
